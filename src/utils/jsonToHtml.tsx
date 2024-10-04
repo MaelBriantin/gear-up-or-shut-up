b74693bd66d003d061103acc59a18af78c5e9c83
MaelBriantin <mael.briantin@proton.me>
2024-10-04 13:58:26 +0200

    feat(json-to-html): refactor jsonToHtml method to handle nested elements consistently

--- src/utils/jsonToHtml.tsx
+++ src/utils/jsonToHtml.tsx
@@ -2,23 +2,10 @@ type JsonElement = {
   htmlTag: string;
   content?: string;
   className?: string;
-  children?: JsonElement[] | { [key: string]: JsonElement };
+  children?: { [key: string]: JsonElement };
 }
 
-export const jsonToHtml = (json: { [key: string]: JsonElement } | JsonElement[], title: string | null = null) => {
-  if (Array.isArray(json)) {
-    return json.map((element, index) => {
-      const { content, htmlTag, className, children } = element;
-      const computedClassName = className ? className : title ? `${title}-${index}` : '';
-      const Tag = htmlTag as keyof JSX.IntrinsicElements;
-      return (
-        <Tag key={index} className={computedClassName}>
-          {content}
-          {children && jsonToHtml(children as JsonElement[])}
-        </Tag>
-      );
-    });
-  } else {
+export const jsonToHtml = (json: { [key: string]: JsonElement }, title: string | null = null) => {
     return Object.keys(json).map(key => {
       const { content, htmlTag, className, children } = json[key];
       const computedClassName = className ? className : title ? `${title}-${key}` : '';
@@ -26,9 +13,8 @@ export const jsonToHtml = (json: { [key: string]: JsonElement } | JsonElement[],
       return (
         <Tag key={key} className={computedClassName}>
           {content}
-          {children && jsonToHtml(children as JsonElement[])}
+          {children && jsonToHtml(children, key)}
         </Tag>
       );
     });
-  }
 };
