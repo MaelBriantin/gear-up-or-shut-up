0d598d862f6b474f6b430b4b6115521c02d7ec64
MaelBriantin <mael.briantin@proton.me>
2024-11-05 23:38:36 +0100

    feat(ui): add a component for options interface button and add the possibility to close the options menu when clicking outside of it

--- src/components/uiComponents/OptionsInterface/index.tsx
+++ src/components/uiComponents/OptionsInterface/index.tsx
@@ -1,27 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { LanguageSelector } from "../LanguageSelector";
 import { ThemeSelector } from "../ThemeSelector";
+import { OptionsInterfaceButton } from "./OptionsInterfaceButton";
 
 export const OptionsInterface = () => {
     const [displayOptions, setDisplayOptions] = useState(false);
+    const containerRef = useRef<HTMLDivElement>(null);
 
     const translateMobile = displayOptions ? 'translate-x-0' : 'translate-x-[200%]';
     const translateDesktop = displayOptions ? 'md:translate-y-0 md:translate-x-0' : 'md:-translate-y-[200%] md:translate-x-0';
 
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (containerRef.current && !containerRef.current.contains(event.target as Node)) {
+                setDisplayOptions(false);
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [containerRef]);
+
     return (
         <div
+            ref={containerRef}
             className="fixed flex flex-col-reverse items-end justify-center w-full gap-2 top-2 right-2 md:top-4 md:right-4 md:flex-row md:justify-end md:items-center md:gap-4">
             <div className={`flex flex-col items-end gap-2 transform transition-transform duration-300 ${translateMobile} ${translateDesktop} md:flex-row md:gap-4`}>
                 <ThemeSelector />
                 <LanguageSelector />
             </div>
-            <div 
-                className={`hover:opacity-100 ${displayOptions ? 'opacity-100' : 'opacity-20'} group flex justify-center items-center px-4 rounded-[4px] h-10 bg-zinc-800 text-white dark:bg-white dark:text-black cursor-pointer text-2xl`}
-                onClick={() => setDisplayOptions(!displayOptions)}>
-                <span className={`${displayOptions ? 'rotate-[-45deg]' : 'rotate-0'} transition-transform duration-50 group-hover:opacity-75 select-none`}>
-                    +
-                </span>
-            </div>
+            <OptionsInterfaceButton displayOptions={displayOptions} setDisplayOptions={setDisplayOptions} />
         </div>
     );
 }
